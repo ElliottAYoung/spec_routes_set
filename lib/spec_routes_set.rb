3748fbcad435922f1b852a64c8ceeedb51eac418
ElliottAYoung <elliott.a.young@gmail.com>
2017-04-19 09:58:22 -0500

    Add connections

--- lib/spec_routes_set.rb
+++ lib/spec_routes_set.rb
@@ -1,3 +1,4 @@
 require "spec_routes_set/version"
-require "spec_routes_set/route_set"
+require "spec_routes_set/spec_route_set"
 require "spec_routes_set/route_appender"
+require "spec_routes_set/route_accessor"
